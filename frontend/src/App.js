89d0142077e45ec4d2bed3a759a39e52f9b3cd7b
dinesh bharati <dineshbharati705@gmail.com>
2025-01-15 09:36:41 +0530

    minor changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import {ToastContainer} from 'react-toastify'
 import Login from './pages/Login';
 import Home from './pages/Home';
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   return (
     <div>
+      <ToastContainer/>
       <Routes>
         <Route path='/' element={<Login/>}/>
         <Route path='/home' element={<Home/>}/>
